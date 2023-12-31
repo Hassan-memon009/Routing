302305d656f856d54e5cfc34fcc3a94a52fecde0
Muhammad Hassan Memon <134962710+Hassan-memon009@users.noreply.github.com>
2023-12-31 21:21:55 +0500

    m

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import Header from './Component/Header'
+import Hero from './Component/Hero'
 
 function App() {
   return (
     <>
       <Header />
+      <Routes>
+        <Route path={'hero'} element={<Hero />} />
+      </Routes>
     </>
   )
 }
--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import { Link } from 'react-router-dom'
 
 function Header() {
-  return <div>Header</div>
+  return (
+    <Navbar expand='lg' className='bg-body-tertiary'>
+      <Container>
+        <Navbar.Brand>
+          <Link to={'/'}>Hassan.</Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='me-auto'>
+            <Nav.Link>
+              <Link to={'hero'}>Hero</Link>
+            </Nav.Link>
+            <Nav.Link href='#link'>Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
 }
 
 export default Header
